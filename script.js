fa3cb4823e09ccad18c08b7025683ea41bd52f4e
Fig Hawkins <fig.hawkins@pcc.edu>
2023-09-10 16:55:11 -0700

    second draft html and css

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const startButton = document.querySelector('.cta-button');
-    startButton.addEventListener('click', changeBackgroundColor);
 
-    function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
 
-    function changeBackgroundColor() {
-        const randomColor = getRandomColor();
-        document.body.style.backgroundColor = randomColor;
-    }
-});
+
